aa71c9541b231e8507b8381c5fd51c5c53a005d7
AnnaKlingenstierna <149709837+AnnaKlingenstierna@users.noreply.github.com>
2024-04-12 11:43:52 +0200

    Updated variabel

--- 02_JS_var_flode_DOM/Anna_Ovning/js/script.js
+++ 02_JS_var_flode_DOM/Anna_Ovning/js/script.js
@@ -1,1 +1,11 @@
-console.log ("Hej")
+
+
+let namn= "Anna";
+let alder= 21;
+let student= true;
+
+alert (`Jag heter ${namn} och är ${alder}`);
+
+console.log (namn,alder,student);
+
+console.log ("Hej");
