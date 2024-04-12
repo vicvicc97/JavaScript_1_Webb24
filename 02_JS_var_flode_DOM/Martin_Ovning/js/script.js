90faa1b70642fdafebcb3bd8065524926ca1b773
martinvrshkoski <mvrskoski@gmail.com>
2024-04-12 14:05:32 +0200

    Created if sats

--- 02_JS_var_flode_DOM/Martin_Ovning/js/script.js
+++ 02_JS_var_flode_DOM/Martin_Ovning/js/script.js
@@ -2,4 +2,17 @@ const firstAndLastName = `Martin Vrshkoski`;
 let age = 21;
 let student = true;
 
-console.log(`My name is ${firstAndLastName}. I am ${age} years old.`)
+/* console.log(`My name is ${firstAndLastName}. I am ${age} years old.`) */
+
+let time = 12;
+
+if (time > 15) {
+    console.log("It is not morning");
+} else  if(time > 17) {
+    console.log("It is night");
+} else if (time === 12) {
+    console.log("It's lunch time");
+} else {
+    console.log("It is something else")
+};
+
