54c587d173b2141922759f3fd51eee3ed7f00805
Atoozeee <atoozeelol@gmail.com>
2024-04-12 11:50:57 +0200

    Variable, console, literal övning

--- 02_JS_var_flode_DOM/EDWINANTONSSON_Ovning/js/script.js
+++ 02_JS_var_flode_DOM/EDWINANTONSSON_Ovning/js/script.js
@@ -1,1 +1,14 @@
-alert("Sorry.")
+let fName = "Edwin Antonsson";
+let age = 21;
+let occ = true;
+let literal = `Mitt namn är ${fName}`;
+
+console.log(fName, age, occ);
+console.log(typeof fName);
+console.log(typeof age);
+console.log(typeof occ);
+
+alert("mitt namn är " + fName);
+alert(literal)
+
+
