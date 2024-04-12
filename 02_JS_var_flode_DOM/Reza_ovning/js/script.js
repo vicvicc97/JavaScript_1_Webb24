de64782927c5ec4676b8befd80c0adbb3e8d147e
Rezardh713 <wrestlers619@gmail.com>
2024-04-12 11:54:20 +0200

    Updated

--- 02_JS_var_flode_DOM/Reza_ovning/js/script.js
+++ 02_JS_var_flode_DOM/Reza_ovning/js/script.js
@@ -1,9 +1,8 @@
-const name = "Reza Doozandeh"
-let age = "19"
+const fullName = "Reza Doozandeh";
+let age = 19;
+const sentence = `Hej mitt namn är ${fullName} och jag är ${age} år gammal.`
+console.log(sentence);
+alert(`Mitt namn är ${fullName} och jag är ${age} år gammal.`);
 
-const sentence = `Hej mitt namn är ${name} och jag är ${age} år gammal.`
 
-console.log(sentence)
-
-alert(sentence)
 
