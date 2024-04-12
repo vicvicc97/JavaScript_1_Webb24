2aab0d31812c3b44fde1f6209f466f6778b843d8
Anes075 <166699658+Anes075@users.noreply.github.com>
2024-04-12 11:41:28 +0200

    Övning 2

---
+++ 02_JS_var_flode_DOM/Anes_Ovning/script/script.js
@@ -0,0 +1,18 @@
+const yourName = "Anes";
+
+let yourAge = "23";
+
+let isStudent = true;
+
+
+const sentence = `Jag heter ${yourName}, ${yourAge} år gammal och är en student`
+
+console.log("Namn: " + yourName);
+
+console.log("Ålder: " + yourAge);
+
+console.log("Är jag en student: " + isStudent);
+
+console.log(sentence);
+
+alert(sentence);
