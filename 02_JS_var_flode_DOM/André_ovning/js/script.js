5489acfdf4d720806a2e35cb08277d5f1b01f255
andys19a <andre.alm@gritacademy.se>
2024-04-12 13:19:05 +0200

    ovning2

--- 02_JS_var_flode_DOM/André_ovning/js/script.js
+++ 02_JS_var_flode_DOM/André_ovning/js/script.js
@@ -1,6 +1,20 @@
-let name = "André";
+const namn = "André";
+let student = false;
+
+console.log(namn);
+console.log(namn);
+
+let sumaType = typeof suma;
 
 let age = "26";
+
+console.log("suma variable type:" + (typeof suma));
+
+console.log(`My name: $(namn)`)
+
+console.log ("suma type: " + sumatype);
+
+console.log (`Namn type: $(namnType), is student? $(isStudent)`)
 
 let student = true;
 
