435b9a78182feb62cfc1723cdd0c2d386637d25a
andys19a <andre.alm@gritacademy.se>
2024-04-12 14:00:00 +0200

    ovning2

--- 01_JS_Intro/js/script.js
+++ 01_JS_Intro/js/script.js
@@ -1,6 +1,18 @@
+<<<<<<< Updated upstream
 
 
 let sum = 2+2;
 
 alert("This is a popup");
 console.log(sum);
+=======
+//one row comment//
+
+let suma = 5+2;
+
+
+console.log (suma);
+
+
+
+>>>>>>> Stashed changes
