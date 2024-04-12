729937f19d2101c50616e02be980b423cc69c648
andys19a <andre.alm@gritacademy.se>
2024-04-12 13:53:16 +0200

    Update script.js

--- 02_JS_var_flode_DOM/André_ovning/js/script.js
+++ 02_JS_var_flode_DOM/André_ovning/js/script.js
@@ -1,35 +1,11 @@
-const namn = "André";
-
-console.log(namn);
-console.log(namn);
-
-let sumaType = typeof suma;
-
-let age = "26";
-
-console.log("suma variable type:" + (typeof suma));
-
-console.log(`My name: $(namn)`)
-
-console.log ("suma type: " + sumatype);
-
-console.log (`Namn type: $(namnType), is student? $(isStudent)`)
-
-let student = true;
-
-if (student = true) {
-
-    alert("André");
-}
 
 
 let time = 17;
 
 if(time > 15){
     alert("it is not morning");
-}
-
-
-if(time < 22){
+}else if(time < 22){
     alert("it is evening");
+}else{
+    alert("is it something else")
 }
