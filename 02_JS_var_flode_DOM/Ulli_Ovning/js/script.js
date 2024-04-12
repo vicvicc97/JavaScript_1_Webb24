11cdad276aa32f603c4bfd6fc18f62b19205c569
Ulli-m <u.mentori@hotmail.com>
2024-04-12 13:19:28 +0200

    ovn 2

--- 02_JS_var_flode_DOM/Ulli_Ovning/js/script.js
+++ 02_JS_var_flode_DOM/Ulli_Ovning/js/script.js
@@ -1,6 +1,6 @@
 
 
-let myname = ` urije`;
+let myname = `Ulli`;
 let myage = 40;
 let text = `mitt namn är ${myname} Jag är ${myage}`;
 let student= true;
@@ -12,4 +12,14 @@ console.log(student);
 
 
 
+let suma= 2+2;
+const name= "Ulli";
+let isStydent= false;
 
+let sumaType = typeof suma;
+let namnType = typeof namn;
+let isStudentType= typeof isStudent;
+
+console.log("suma variable type: " + ( typeof suma));
+console.log(`My name: ${namn})`);
+console.log("Suma")
