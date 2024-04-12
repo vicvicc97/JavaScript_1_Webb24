8cdaab9796b2b55a24c335e5b9e228abc3ff76bf
martinvrshkoski <mvrskoski@gmail.com>
2024-04-12 15:21:49 +0200

    Updated script.js

--- 02_JS_var_flode_DOM/Martin_Ovning/js/script.js
+++ 02_JS_var_flode_DOM/Martin_Ovning/js/script.js
@@ -13,6 +13,27 @@ if (time > 15) {
 } else if (time === 12) {
     console.log("It's lunch time");
 } else {
-    console.log("It is something else")
-};
+    console.log("It is something else");
+}
+
+let hour = 8;
+
+if (hour > 5 || hour < 10) {
+    console.log("Hour is between 5 and 10");
+} 
+
+if ( !(hour < 5)) {
+    console.log("It is crazy");
+}
+
+
+i = 10;
+
+if (i > 10) {
+    console.log("Bigger");
+} else if (i < 10) {
+    console.log("Smaller")
+} else {
+    console.log("Give up!!!");
+}
 
