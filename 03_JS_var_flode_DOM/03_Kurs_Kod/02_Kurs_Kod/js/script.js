8b50efec69e1955290099dde10bdf9e9f3376af7
LukasKurasinski <146763137+LukasKurasinski@users.noreply.github.com>
2024-04-15 23:13:30 +0200

    some example code

--- 03_JS_var_flode_DOM/03_Kurs_Kod/02_Kurs_Kod/js/script.js
+++ 03_JS_var_flode_DOM/03_Kurs_Kod/02_Kurs_Kod/js/script.js
@@ -14,9 +14,9 @@ for (let index = 0; index < 10; index++) { //<- index ++ is the same as i = i + 
     console.log("index = " + index);
 }
 console.log("Stoping loop index = 10");
- 
 
-  
+
+
 console.log("----------------------------------------------------------------------------------------");
 
 
@@ -40,3 +40,54 @@ console.log("-------------------------------------------------------------------
 
 
 
+// nested loops
+console.log(`
+        for (let i = 0; i < 10; i++) {
+            for (let j = 0; j < 10; j++) {
+                console.log("i = " + i + " : j = " + j);
+            }
+        }
+    `);
+
+for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+        console.log("i = " + i + " : j = " + j);
+    }
+}
+
+
+
+
+console.log("----------------------------------------------------------------------------------------");
+
+
+
+// complex loop
+console.log(`
+        for (let i = 0; i < 10; i++) {
+            let printString = "";
+            for (let j = i; j >= 0; j--) {
+                printString = printString + "*";
+            }
+            console.log(printString);
+        }
+    `);
+
+for (let i = 0; i < 10; i++) {
+    let printString = "";
+    for (let j = i; j >= 0; j--) {
+        printString = printString + "*";
+    }
+    console.log(printString);
+}
+
+//DOM-----------------------------------------------------------
+
+let h1 = document.getElementById("h1");
+
+
+for (let index = 0; index < 10; index++) {
+    let p = document.createElement("p");
+    p.textContent = index;
+    h1.appendChild(p);
+}
