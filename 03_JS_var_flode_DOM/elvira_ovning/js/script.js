aa6ee3e7cde8515419d1b84836d3934c89125560
Elvira Pilblad <elvirapihlblad@hotmail.com>
2024-04-16 11:34:51 +0200

    ÖvningUppdatering

--- 03_JS_var_flode_DOM/elvira_ovning/js/script.js
+++ 03_JS_var_flode_DOM/elvira_ovning/js/script.js
@@ -2,16 +2,16 @@ let bobAge=30;
 let robAge=25;
 let zobAge=35;
 
-if (bobAge > robAge || zobAge){
-    console.log("Bob är äldst")
+if (bobAge > robAge && bobAge > zobAge){
+    console.log("Bob är äldst");
+} else if (robAge > bobAge && robAge > zobAge){
+    console.log("Rob är äldst");
+} else if (zobAge > bobAge && zobAge > robAge){
+    console.log("Zob är äldst");
 }
-else if (robAge > bobAge || zobAge){
-    console.log("Rob är äldst") }
-else if (zobAge > bobAge || robAge){
-    console.log("Zob är äldst") }
-else if (zobAge == bobAge > robAge){
-    console.log("Det är lika mellan vissa personer")
-}
-else(zobAge && robAge && bobAge){
-    console.log("Det har alla samma ålder")
+
+if (bobAge === robAge && bobAge === zobAge && robAge === zobAge) {
+    console.log("Det har alla samma ålder");
+} else if (bobAge === robAge || bobAge === zobAge || robAge === zobAge) {
+    console.log("Det är lika mellan vissa personer");
 }
