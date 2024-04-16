f92f7ebb50c1e64c326dbbccbe2bf5f72daa07d6
Ulli-m <u.mentori@hotmail.com>
2024-04-16 11:57:15 +0200

    Ulli

--- 03_JS_var_flode_DOM/Ulli_Ovning/Js/script.js
+++ 03_JS_var_flode_DOM/Ulli_Ovning/Js/script.js
@@ -7,27 +7,27 @@ let zobAge = 35;
 
 if (bobAge > robAge && bobAge > zobAge){
 
-    console.log ( "Bob är äldst")
+    console.log ( "Bob är äldst");
     
 }
 
 else if (robAge > bobAge && robAge > zobAge){
 
-    console.log ( "Rob är äldst")
+    console.log ( "Rob är äldst");
 }
 else if ( zobAge > bobAge && zobAge > robAge){
 
-console.log("Zob är äldst")
+console.log("Zob är äldst");
 
 }
 
-else if ( bobAge == robAge && robAge == zobAge && zobAge == bobAge ){
+ if ( bobAge === robAge || robAge === zobAge || zobAge === bobAge ){
 
-    console.log("Det är lika mellan vissa personer.")
+    console.log("Det är lika mellan vissa personer.");
 }
 
-else if ( bobAge == robAge == zobAge ){
+else ( bobAge === robAge && bobAge === zobAge && robAge === zobAge ){
 
-    console.log ( "De har alla samma ålder")
+    console.log ( "De har alla samma ålder");
 
 }
