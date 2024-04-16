d605a06bf76113919df6af23caa0b575575cdd50
LeNa24Shi <lena.shihab@gritacademy.se>
2024-04-16 10:49:53 +0200

    if else Övning

---
+++ 03_JS_var_flode_DOM/Lena_Ovning/Script/script.js
@@ -0,0 +1,18 @@
+
+
+let bobAge=30
+let robAge=25
+let zobAge=35
+
+if (bobAge > robAge && bobAge > zobAge) {
+    console.log("Bob är äldst")
+}
+else if (robAge > bobAge && robAge > zobAge) {
+    console.log("Rob är äldst")
+}
+else if (zobAge > bobAge && zobAge > robAge) {
+    console.log("Zob är äldst")
+}
+else if (zobAge === robAge && robAge === bobAge) {
+    console.log("De har alla samma ålder")
+}
