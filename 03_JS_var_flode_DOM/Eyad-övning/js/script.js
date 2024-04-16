edb9851f73a73a78fb7d519acf6456e212f0ad8c
Eyadho <153440589+Eyadho@users.noreply.github.com>
2024-04-16 12:35:42 +0200

    Update script.js

--- 03_JS_var_flode_DOM/Eyad-övning/js/script.js
+++ 03_JS_var_flode_DOM/Eyad-övning/js/script.js
@@ -12,6 +12,6 @@ if (zobAge > bobAge && zobAge > robAge) {
 }
 if (bobAge == robAge && robAge == zobAge && zobAge == bobAge) {
     console.log("De har alla samma ålder");
-} else if(bobAge == zobAge || zobAge == robAge){
+} else if(bobAge == zobAge || zobAge == robAge || bobAge == robAge){
     console.log("Det är lika mellan vissa personer");
 }
