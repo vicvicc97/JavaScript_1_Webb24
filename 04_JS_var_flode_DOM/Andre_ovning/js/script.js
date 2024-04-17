ef1124bf29daf56673071e32b1d1a05bd031959e
andys19a <andre.alm@gritacademy.se>
2024-04-17 13:11:15 +0200

    Ovning4

---
+++ 04_JS_var_flode_DOM/Andre_ovning/js/script.js
@@ -0,0 +1,15 @@
+for (let row = 1; row <= 5; row++) {
+    let cell = "";
+    for (let col = 1; col <= 5; col++) {
+        cell += "|" + row * col + "| \t";
+    }
+    console.log(cell);
+}
+
+
+
+
+
+
+
+
