f99b4920fc80c8726fdd3d3d75f5e6fb71463604
SanjaK04 <159773919+SanjaK04@users.noreply.github.com>
2024-04-17 10:45:33 +0200

    Update script.js

---
+++ 04_JS_var_flode_DOM/04_Kurs_Kod/Sanja_övning/JS/script.js
@@ -0,0 +1,14 @@
+
+
+
+for (let row=0; row<5; row++) {
+
+    let cell="";
+
+    for (let col=0; col<5; col++) {
+
+        cell = cell + "|" + row + " " + col + "|";
+    }
+
+    console.log(cell);
+}
