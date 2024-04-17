c4e8ed5108c13f2ca5e37c873b4788b283f31b09
AnnaKlingenstierna <149709837+AnnaKlingenstierna@users.noreply.github.com>
2024-04-17 09:06:15 +0200

    Update script.js

--- 03_JS_var_flode_DOM/Anna_övning/Js/script.js
+++ 03_JS_var_flode_DOM/Anna_övning/Js/script.js
@@ -5,42 +5,48 @@ let bobAge = 30;
 let robAge = 25;
 let zobAge = 35;
 
-if (bobAge > robAge && bobAge > zobAge) {
-    console.log("Bob är äldst");
-}; 
-
-
-if (robAge > bobAge && robAge > zobAge) {
-    console.log ("Rob är äldst")
+if (bobAge > robAge && bobAge > zobAge) { /* Äldre än båda */
+    console.log ("Bob är äldst!");
 }
 
-if (zobAge > bobAge && zobAge > robAge) {
+else if (robAge > bobAge && robAge > zobAge) {
+    console.log ("Rob är äldst!");
+}
+
+else if (zobAge > bobAge && zobAge > robAge) {
     console.log ("Zob är äldst!");
 }
 
-
-if (bobAge== robAge && bobAge > robAge && zobAge) {
-    console.log ("Det är lika mellan olika personer! Rob och Zob är äldre än Bob!");
+if (bobAge === robAge  ||  bobAge===zobAge   ||robAge===zobAge) {
+    console.log("Det är lika mellan vissa personer")
 }
 
-else if (bobAge == zobAge && robAge > bobAge && zobAge > bobAge)
- {
-    console.log ("Det är lika mellan olika personer! Bob och Zob är äldre än Rob!")
- }
+else if (bobAge && robAge && bobAge && zobAge  && robAge && zobAge){
 
-else if (robAge == zobAge && robAge > bobAge && zobAge >bobAge)
-{
-    console.log ("Det är lika mellan olika personer! Rob och Zob är äldre än Bob!")
+} ;
+
+
+
+
+
+
+/* FOR loops */
+
+
+for (let index = 0; index < 10; index++ ) { /* 1-9 */
+    console.log (index);
+
+    if (index === 9) {
+        console.log (" Första klar")
+    }
+} 
+ 
+
+ for (let i = 1; i < 52; i++) { /* 0-50 jämna tal */
+
+    if (i%2 === 0) {
+        console.log(i) ;
+    }
 }
 
-
-
-
-if (bobAge == robAge && bobAge == zobAge && robAge == zobAge)
-
-{
-    console.log ("De har alla samma ålder");
-}
-
-
-
+   
