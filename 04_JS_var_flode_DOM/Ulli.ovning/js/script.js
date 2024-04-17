2d603d30bb399e79979c5d7d2f8b3198e3f15d47
Ulli-m <u.mentori@hotmail.com>
2024-04-17 15:32:28 +0200

    ulli

---
+++ 04_JS_var_flode_DOM/Ulli.ovning/js/script.js
@@ -0,0 +1,25 @@
+/*
+for (let row = 1; row < 6; row++) {
+    let cell ="";
+
+    for (let col = 1; col < 6; col++) {
+        cell += "|" + row * col + "| \t" ;
+    }
+    console.log(cell);
+}
+
+*/
+
+let body =  document.querySelector("body");
+body.style.backgroundColor = "rgb(211,211,211)";
+
+
+
+let myHeader =  document.querySelector("h1");
+myHeader.innerText = "this is the new header text";
+
+
+
+let myParaghraph =  document.querySelector("p");
+myParaghraph.innerText = "this is the new paragraph";
+
