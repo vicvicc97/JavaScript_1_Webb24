35a7b59e5c4619311a881e1af9c719f3f7f3f172
martinvrshkoski <mvrskoski@gmail.com>
2024-04-18 12:09:49 +0200

    Updated script.js
    Created index content with js

--- 04_JS_var_flode_DOM/Martin_Ovning/js/script.js
+++ 04_JS_var_flode_DOM/Martin_Ovning/js/script.js
@@ -1,3 +1,4 @@
+/* 
 console.log("---------------------------------");
 
 for (let row = 1; row <= 5; row++) {
@@ -20,10 +21,11 @@ for (let i = 0; i <= 100; i++) {
 
 console.log(sum);
 
-console.log("---------------------------------");
+console.log("---------------------------------"); 
+*/
 
 /* F2 shortcut för att ändra till exempel alla variabler med samma namn  */
-
+/* 
 let body = document.querySelector("body");
 
 body.style.backgroundColor = "green";
@@ -35,5 +37,66 @@ p.innerText += "Hello World";
 h1.innerText += "Header";
 
 body.appendChild(h1)
-body.appendChild(p)
+body.appendChild(p) 
+*/
 
+console.log("---------------------------------");
+
+let body = document.querySelector("body");
+
+let table = document.createElement("table");
+table.style.border = "2px solid black"
+table.style.width = "50%";
+table.style.height = "100px";
+table.style.margin = "30vh auto 0 auto"
+table.style.borderCollapse = "collapse";
+
+let rowCount = 5;
+let colCount = 5;
+
+for (let row = 0; row < rowCount; row++) {
+    let tableRow = document.createElement("tr");
+    tableRow.style.border = "30px"
+    tableRow.style.width = "90%";
+    table.appendChild(tableRow);
+
+
+    for (let col = 0; col < colCount; col++) {
+        if (row === 0) {
+            let tableHeader = document.createElement("th");
+            tableHeader.innerText = "Header";
+            tableRow.appendChild(tableHeader);
+        } else {
+            let tableData = document.createElement("td");
+            tableData.setAttribute("id", `${row}${col}`);
+            tableData.innerText = col;
+            tableData.style.border = "1px solid black";
+            tableData.style.padding = "5px";
+            tableData.style.textAlign = "center";
+            tableRow.appendChild(tableData);
+        }
+        
+    }
+
+}
+
+body.appendChild(table);
+
+for (let row = 0; row < rowCount; row++) {
+    for (let col = 0; col < colCount; col++) {
+        if (row !== 0) {
+            document.getElementById(`${row}${col}`).innerText = row;
+        }
+    }
+    
+}
+
+document.getElementById("34").innerText = "text"
+
+let headers = document.getElementsByTagName("th");
+let headersData = ["name", "job", "bil", "cat", "dog"];
+
+for (let index = 0; index < headers.length; index++) {
+    headers[index].innerText = headersData[index];
+    
+}
