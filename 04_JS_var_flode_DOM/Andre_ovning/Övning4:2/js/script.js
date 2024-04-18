f2c8ddeada1556bc21fd5c5fcba1f379760b644b
andys19a <andre.alm@gritacademy.se>
2024-04-18 08:57:40 +0200

    ovning4

---
+++ 04_JS_var_flode_DOM/Andre_ovning/Övning4:2/js/script.js
@@ -0,0 +1,5 @@
+let body = document.querySelector("body");
+body.innerHTML = body.innerHTML + "<h1> easy peasy</h1>" + "<p> Hurray</p>"
+
+//Change body background color
+body.style.backgroundColor = "lightgrey";
