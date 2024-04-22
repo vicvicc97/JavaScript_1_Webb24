9899819d3f2281a7a9d4357c9e6ef4f57b1334a6
Vic <84406813+vicvicc97@users.noreply.github.com>
2024-04-22 09:08:14 +0200

    Update html.js

--- 04_JS_var_flode_DOM/Victor_Ovning/js/html.js
+++ 04_JS_var_flode_DOM/Victor_Ovning/js/html.js
@@ -1,12 +1,20 @@
-
-
 //get body ref from HTML
 let documentBody = document.getElementById("body");
+
+
+
+
+//body styling
+document.getElementById("body").style.textAlign = "center";
 
 //create table element
 let table = document.createElement("table");
 table.style.border = "1px solid black";
 table.style.width = "25px";
+
+table.setAttribute("id", "table")
+
+
 
 let rowCount = 10;
 let colCount = 5;
@@ -29,7 +37,7 @@ for (let row = 0; row < rowCount; row++) {
             tableData.innerText = col;
             tableRow.appendChild(tableData);
         }
-    }
+    } 
 
     for (let row = 0; row < rowCount; row++) {
         for (let col = 0; col < colCount; col++);
@@ -38,16 +46,34 @@ for (let row = 0; row < rowCount; row++) {
 
 //append table to HTML body
 documentBody.appendChild(table);
+document.getElementById("table").style.marginLeft = "auto";
+document.getElementById("table").style.marginRight = "auto";
+document.getElementById("table").style.width = "50%";
 
-document.getElementById("34").innerText = "TEXT";
+document.getElementById("34").innerText = "boop";
 
-document.getElementById("14").innerText = "beep";
+document.getElementById("33").innerText = "beep";
 
 
 //article element
 let article = document.createElement("article");
 article.style.border = "1px solid black";
 article.style.width = "25px";
+
+documentBody.appendChild(article);
+
+
+body.innerHTML = body.innerHTML + "<h1>Article</h1>" + "<p>Paragraph</p>" + "<p>Paragraph</p>";
+
+body.style.backgroundColor = "";
+
+body.innerHTML = body.innerHTML + "<h1>Article</h1>" + "<p>Paragraph</p>" + "<p>Paragraph</p>";
+
+body.style.backgroundColor = "";
+
+
+
+
 
 
 
