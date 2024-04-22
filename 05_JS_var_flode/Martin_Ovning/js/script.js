dd801911ca1032193056237e4b7dd7cd4313e83d
martinvrshkoski <mvrskoski@gmail.com>
2024-04-22 15:40:41 +0200

    Updated script.js

--- 05_JS_var_flode/Martin_Ovning/js/script.js
+++ 05_JS_var_flode/Martin_Ovning/js/script.js
@@ -38,3 +38,58 @@ console.log(convertNumber);
 
 console.log("--------------------------------");
 
+/* fornamn.concat('', efternamn); */
+
+console.log("HELLO".toLowerCase());
+
+let text = "HELLO";
+console.log("Start string: " + text);
+
+let textToLowerCase = text.toLowerCase();
+console.log("Start string toLowerCase: " + textToLowerCase);
+
+let textToUpperCase = textToLowerCase.toUpperCase();
+console.log("Start string to UpperCase: " + textToUpperCase);
+
+
+let strang = "JavaScript är sämst";
+
+console.log(strang.substring(0, 4));
+console.log(strang.slice(4));
+
+console.log(strang.indexOf("är")); 
+console.log(strang.includes("är")); 
+
+console.log("-------------------------------");
+
+let badInputText = "   hello   ";
+console.log("bad text; "+ badInputText);
+let trimedText = badInputText.trim();
+console.log("Trimed Text; "+ trimedText);
+
+
+console.log("-------------------------------");
+console.log("-------------------------------");
+
+
+let userData = "Bob, Rob ,  Zob";
+let userDataSplit = userData.split(",");
+console.log(userData);
+for (let i = 0; i < userDataSplit.length; i++) {
+    userDataSplit[i] = userDataSplit[i].trim();
+}
+console.log(userDataSplit);
+
+let userDataJoin = userDataSplit.join(", ");
+console.log("Joining user data split with ', ' ");
+console.log(userDataJoin);
+
+
+console.log("-------------------------------");
+
+let message = "Hej John";
+let newMessage = message.replace("John", "Johnny");
+console.log(newMessage);
+
+
+console.log("-------------------------------");
