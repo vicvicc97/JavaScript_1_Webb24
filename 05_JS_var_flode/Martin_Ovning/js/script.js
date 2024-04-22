4ee1ef7ed743c4f7dd165e57171ea2122956ae53
martinvrshkoski <mvrskoski@gmail.com>
2024-04-22 11:58:25 +0200

    Update script.js

---
+++ 05_JS_var_flode/Martin_Ovning/js/script.js
@@ -0,0 +1,40 @@
+/* 
+Math.pow();
+Math.random();
+ */
+
+/* 
+console.log("--------------------------------");
+
+console.log(Math.random());
+console.log("\n");
+console.log(Math.round(1.687));
+
+console.log("--------------------------------");
+
+// Date and Time
+let currentDate = new Date();
+console.log(currentDate);
+
+console.log(currentDate.getFullYear());
+ */
+
+console.log("--------------------------------");
+
+// Strängar
+
+/* console.log(stringNumber.toString()); */
+
+let stringNumber = 5;
+let convertNumber;
+
+if (Number.isInteger(stringNumber)) {
+    convertNumber = stringNumber.toString();
+}
+console.log(convertNumber);
+
+
+
+
+console.log("--------------------------------");
+
