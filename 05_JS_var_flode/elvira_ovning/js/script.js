3d3919980ccb648d3ec0729739ab8fb4dc428359
Elvira Pilblad <elvirapihlblad@hotmail.com>
2024-04-22 13:01:29 +0200

    Update05

---
+++ 05_JS_var_flode/elvira_ovning/js/script.js
@@ -0,0 +1,8 @@
+console.log("Power: " + (Math.pow(10,2)));
+console.log("Random: " + Math.random());
+console.log("Random: " + Math.random()*10);
+console.log("");
+console.log("");
+console.log("");
+console.log("");
+console.log("");
