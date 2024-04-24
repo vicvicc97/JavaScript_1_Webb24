3d67a79ed45be614393d6999921379e296970bb8
LukasKurasinski <146763137+LukasKurasinski@users.noreply.github.com>
2024-04-24 11:09:55 +0200

    fix ecersise

--- 07_Funktioner_metoder_events/07_Kurs_Kod/js/script.js
+++ 07_Funktioner_metoder_events/07_Kurs_Kod/js/script.js
@@ -11,6 +11,24 @@ function outerFunc() {
 let closureEx = outerFunc(); //outerFunc() return reference to innerFunc()
 closureEx(); // this will log num
 
+//funktioner ovning
+    //Skapa tre funktioner för att hantera maträtter. Istället för att använda listor eller arrayer kommer vi att använda funktioner för att spara och manipulera data
+
+    //en funktion för att lägga till en maträtt (counter++)
+let counter = 0; //represent dish count
+
+function addDish(){
+    counter++;
+}
+    //en funktion för att räkna antalet tillagda maträtter (log(counter)) 
+fun
+
+addDish();
+addDish();
+addDish();
+addDish();
+
+
 // onclick example example
 
 
