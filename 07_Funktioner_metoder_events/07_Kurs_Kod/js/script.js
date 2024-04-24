64a80e33f73822e92fd1263235b0b03602941561
LukasKurasinski <146763137+LukasKurasinski@users.noreply.github.com>
2024-04-24 10:22:18 +0200

    added scope example

--- 07_Funktioner_metoder_events/07_Kurs_Kod/js/script.js
+++ 07_Funktioner_metoder_events/07_Kurs_Kod/js/script.js
@@ -1,3 +1,16 @@
+//scope example
+
+function outerFunc() {
+  let num = 10;
+  function innerFunc() {//this function is not avaliable in the outer scope
+    console.log(num); // ​
+  }
+  return innerFunc;
+}
+//outerFunc(); 
+let closureEx = outerFunc(); //outerFunc() return reference to innerFunc()
+closureEx(); // this will log num
+
 // onclick example example
 
 
